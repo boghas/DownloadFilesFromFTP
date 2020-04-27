649b633b06c698d1bd34a60bbb64616f03301ac1
boghas <58976378+boghas@users.noreply.github.com>
2020-04-27 16:29:39 +0300

    The script

--- downloadFromFTP.py
+++ downloadFromFTP.py
@@ -10,6 +10,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
+
 class FTPServer(object):
 	def __init__(self, server, user, password):
 		self.server = server
@@ -20,7 +21,7 @@ class FTPServer(object):
 		try:
 			ftp = ftplib.FTP(self.server)
 			ftp.login(self.user, self.password)
-			ftp.cwd('SRS_EFT_FILES')
+			ftp.cwd('Desired_directory')
 			print('connected')
 			return ftp
 		except:
@@ -29,7 +30,7 @@ class FTPServer(object):
 
 	def downloadFiles(self, ftp):
 		file_dict = {}
-		os.chdir('C:\\Users\\bogdan.hasan\\Desktop\\pyDBTEST\\KSH scripting\\files\\')
+		os.chdir('directory_to_download')
 		for f in ftp.nlst():
 			with open(f, 'wb') as fp:
 				ftp.retrbinary('RETR ' + f, fp.write)
@@ -39,38 +40,28 @@ class FTPServer(object):
 	def getEmailRecipients(self,file_path):
 		with open(file_path) as myFile:
 			for line in myFile:
-				if("SRS_EFT_EMAIL_RECIPIENTS=" in line and not line.startswith('#')):
+				if("EMAIL_RECIPIENTS=" in line and not line.startswith('#')):
 					pos = line.find('=')
 					li = line[pos+1:len(line)-2].split('-t ')
 		li.pop(0)
 		return li
 
 
-# First create an FTPServer object
-# Create a connection to the FTP server
-# Download the files from the FTP server
-# Using the connection we just created
-# Get the list of the mail recipients
-'''
-ftps = FTPServer(server, user, password)
-connection = ftps.connect()
-ftps.downloadFiles(connection)
-recipients = ftps.getEmailRecipients(file_with_emails)
-'''
 
-server = 'transfer.dufry.com'
-user = 'ftp_uk_fi_sap'
-password = 'Chained_42m7'
-downloadTo = 'D:\\DIPDATA\\EFT File Transfer\\files\\'
-backup_folder = 'D:\\DIPDATA\\EFT File Transfer\\bckup\\'
-file_with_emails = 'D:\\DIPDATA\\EFT File Transfer\\files\\\\text.txt'
+
+server = 'server_name'
+user = 'username'
+password = 'password'
+downloadTo = 'path_to_destination_folder'
+backup_folder = 'path_to_backup_folder'
+file_with_emails = 'path_to_file_with_emails'
 file_dict = {}
 
-sender = 'ops@6k3.dufry.com'
-receiver = 'bogdan.hasan@mindit.io'
+sender = 'sender@domain'
+receiver = 'receiver@domain'
 
 msg = MIMEMultipart()
-msg['Subject'] = 'EFT files'
+msg['Subject'] = 'Subject'
 msg['From'] = sender
 msg['To'] = receiver
 
@@ -80,7 +71,7 @@ text = '''\
 	<title></title>
 </head>
 <body aria-readonly='false'>Dear All,<br><br>
-Please find attached the Consolidated and Summary EFT files.<br>
+Please find attached the files.<br>
 '''
 
 for f in Path(downloadTo).iterdir():
@@ -89,18 +80,11 @@ for f in Path(downloadTo).iterdir():
 	encoders.encode_base64(part)
 	part.add_header('Content-Disposition', 'attachment; filename=' + f.name)
 	msg.attach(part)
-	text += 'Attached you can find the EFT file ' + f.name + ' <br>'
+	text += 'Attached you can find the downloaded file: ' + f.name + ' <br>'
 
 msg.attach(MIMEText(text,'html'))
 smtpObj = smtplib.SMTP('172.22.50.25',25)
 smtpObj.sendmail(sender, receiver, msg.as_string())
-print("Email with reconciliation files sent!\n")
-'''
-for path in Path(downloadTo).iterdir():
-	try:
-		shutil.move(str(path), backup_folder)
-		print('file ' + path.name + " moved to backup folder")
-	except:
-		print('file ' + path.name + " failed to be moved to backup folder")
-'''
+print("Email sent!\n")
+
 
